ba824c486b10bb0bc8d1c53cf04d6ae3945e3541
Serghio07 <ticona465@gmail.com>
2024-11-04 19:45:16 -0400

    contac1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +10,19 @@ def home():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/servicios')
+def servicios():
+    return render_template('Servicios.html')
+
+@app.route('/nosotros')
+def about():
+    return render_template('Sobre_Nosotros.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
