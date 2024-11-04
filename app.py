2c1d356d6df49c545ceb63288c6f7ba4494cedd7
Ismael Rivera <ismael.rivera.work@gmail.com>
2024-11-04 16:46:06 -0400

    Cambios BAse y front end

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser:mypassword@localhost:3306/mydatabase'
 db = SQLAlchemy(app)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('service.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 @app.route('/contact')
 def contact():
@@ -15,5 +27,3 @@ def contact():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
